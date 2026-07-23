82ae14e208b87809d0e31b7be6976f03f0ad16b5
Murilo Alves <murilo.alves@flashapp.com.br>
2026-07-23 15:22:27 -0300

    fix(talkactions): teleport player directly to destination on /tp and /teleport command

--- data/scripts/talkactions/gm/teleport_set_destination.lua
+++ data/scripts/talkactions/gm/teleport_set_destination.lua
@@ -17,12 +17,16 @@ function teleportSetDestination.onSay(player, words, param)
 		position:getNextPosition(player:getDirection(), 1)
 		local x, y, z = tonumber(params[1]), tonumber(params[2]), tonumber(params[3])
 		local destination = (x and y and z) and Position(x, y, z) or nil
-		if destination and destination:getTile() then
+		if destination then
+			player:teleportTo(destination)
+			if not player:isInGhostMode() then
+				destination:sendMagicEffect(CONST_ME_TELEPORT)
+			end
 			local tp = Game.createItem(35502, 1, position)
 			if tp then
 				tp:setDestination(destination)
-				player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("New position: %s", param))
 			end
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("New position: %s", param))
 		else
 			player:sendCancelMessage("Destination position is not valid.")
 		end
